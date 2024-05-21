19c192cf30070167bcac69dc419dedf02b491338
Edzer Dionido <jeddionido26@gmail.com>
2024-05-21 11:38:55 +0800

    add seeder contents

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
+use Database\Factories\ReviewFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        Book::factory(33)->create()->each(function($book) {
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)->good()->for($book)->create();
+        });
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
