829c5bcbe98a4113a4df5e5e23fc158250367360
Edzer Dionido <jeddionido26@gmail.com>
2024-05-21 11:38:41 +0800

    add seeder contents

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'book_id' => null,
+            'stars' => fake()->numberBetween(1,5),
+            'content' => fake()->paragraph(),
+            'created_at' => fake()->dateTimeBetween('-20 years'), 
+            'updated_at' => fake()->dateTimeBetween('created_at', 'now') 
+        ];
+    }
+
+    public function good() 
+    {
+        return $this->state(function(array $attribute){
+            return [
+                'stars' => fake()->numberBetween(3,5)
+            ];
+        });
+    }
+    
+    public function average() 
+    {
+        return $this->state(function(array $attribute){
+            return [
+                'stars' => fake()->numberBetween(3,5)
+            ];
+        });
+    }
+
+    public function bad() 
+    {
+        return $this->state(function(array $attribute){
+            return [
+                'stars' => fake()->numberBetween(1,3)
+            ];
+        });
+    }
+}
